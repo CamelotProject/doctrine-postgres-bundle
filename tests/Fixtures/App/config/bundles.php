ef6627195db8368d65aa4a8f5f15f39669a545b9
Gawain Lynch <gawain.lynch@gmail.com>
2019-12-04 07:12:02 +0100

    Drop dev require on doctrine/doctrine-cache-bundle

--- tests/Fixtures/App/config/bundles.php
+++ tests/Fixtures/App/config/bundles.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
